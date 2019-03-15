6f549c22faa7c6dfa76d59f44d1320b7e05a5622
Jirka Kremser <jkremser@redhat.com>
2019-03-15 10:33:26 +0100

    wip

--- src/main/java/io/radanalytics/operator/Entrypoint.java
+++ src/main/java/io/radanalytics/operator/Entrypoint.java
@@ -1,10 +1,7 @@
 package io.radanalytics.operator;
 
 import com.jcabi.manifests.Manifests;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.Watch;
+import io.fabric8.kubernetes.client.*;
 import io.fabric8.kubernetes.client.utils.HttpClientUtils;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.exporter.HTTPServer;
@@ -13,10 +10,7 @@ import io.prometheus.client.log4j.InstrumentedAppender;
 import io.radanalytics.operator.common.AbstractOperator;
 import io.radanalytics.operator.common.AnsiColors;
 import io.radanalytics.operator.common.OperatorConfig;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.reflections.Reflections;
 import org.reflections.ReflectionsException;
 import org.reflections.scanners.ResourcesScanner;
@@ -27,8 +21,14 @@ import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.IntStream;
@@ -47,45 +47,61 @@ public class Entrypoint {
 
     private static final Logger log = LoggerFactory.getLogger(Entrypoint.class.getName());
 
-    public static ExecutorService EXECUTORS = Executors.newFixedThreadPool(10);
-
     private static OperatorConfig config;
     private static KubernetesClient client;
+    private static ExecutorService executors;
 
     public static void main(String[] args) {
         log.info("Starting..");
         config = OperatorConfig.fromMap(System.getenv());
+//        Config c = new Config(String, String, String, boolean, boolean, String, String, String, String, String,
+// String, String, String , String , String , String , int , int , int , int , long , long , int , int , String ,
+// String , String[] , Map<Integer, String> , String , TlsVersion[] , long , long , String , String , String ,
+// String , String , String , String , String[] , Map<String, List<String>> );
+//        Config c = new Config(null, null, null, false, false, null, null, null, null, null, null, null, null, null, null);
+//        new ConfigBuilder
+//        client = new DefaultKubernetesClient(getOkHttpClient(),);
         client = new DefaultKubernetesClient();
         boolean isOpenshift = isOnOpenShift();
+        log.error("cajk0");
         CompletableFuture<Void> future = run(isOpenshift).exceptionally(ex -> {
             log.error("Unable to start operator for one or more namespaces", ex);
             System.exit(1);
             return null;
         });
         if (config.isMetrics()) {
-            CompletableFuture<Optional<HTTPServer>> maybeMetricServer = future.thenCompose(s -> runMetrics(isOpenshift));
+//            CompletableFuture<Optional<HTTPServer>> maybeMetricServer = future.thenCompose(s -> runMetrics(isOpenshift));
         }
+        log.error("cajk1");
+        future.join();
+        log.error("cajk2");
     }
 
     private static CompletableFuture<Void> run(boolean isOpenShift) {
         printInfo();
-
+        log.error("222cajk1");
         if (isOpenShift) {
             log.info("{}OpenShift{} environment detected.", AnsiColors.ye(), AnsiColors.xx());
         } else {
             log.info("{}Kubernetes{} environment detected.", AnsiColors.ye(), AnsiColors.xx());
         }
+        log.error("cajkaa");
 
         List<CompletableFuture> futures = new ArrayList<>();
         if (SAME_NAMESPACE.equals(config.getNamespaces().iterator().next())) { // current namespace
+            log.error("cajk3");
             String namespace = client.getNamespace();
             CompletableFuture future = runForNamespace(isOpenShift, namespace, config.getReconciliationIntervalS(), 0);
+            log.error("cajk4");
             futures.add(future);
         } else {
             if (ALL_NAMESPACES.equals(config.getNamespaces().iterator().next())) {
+                log.error("cajk5");
                 CompletableFuture future = runForNamespace(isOpenShift, ALL_NAMESPACES, config.getReconciliationIntervalS(), 0);
+                log.error("cajk6");
                 futures.add(future);
             } else {
+                log.error("cajk7");
                 Iterator<String> ns;
                 int i;
                 for (ns = config.getNamespaces().iterator(), i = 0; i < config.getNamespaces().size(); i++) {
@@ -94,46 +110,63 @@ public class Entrypoint {
                 }
             }
         }
+        log.error("cajk8");
         return CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{}));
     }
 
-    private static CompletableFuture<Optional<HTTPServer>> runMetrics(boolean isOpenShift) {
-        HTTPServer httpServer = null;
-        try {
-            log.info("Starting a simple HTTP server for exposing internal metrics..");
-            httpServer = new HTTPServer(config.getMetricsPort());
-            log.info("{}metrics server{} listens on port {}", AnsiColors.ye(), AnsiColors.xx(), config.getMetricsPort());
-            // todo: create also the service and for openshift also expose the service (?)
-        } catch (IOException e) {
-            log.error("Can't start metrics server because of: {} ", e.getMessage());
-            e.printStackTrace();
-        }
-        if (config.isMetricsJvm()) {
-            DefaultExports.initialize();
-        }
-        final Optional<HTTPServer> maybeServer = Optional.of(httpServer);
-        return CompletableFuture.supplyAsync(() -> maybeServer);
-    }
+//    private static CompletableFuture<Optional<HTTPServer>> runMetrics(boolean isOpenShift) {
+//        HTTPServer httpServer = null;
+//        try {
+//            log.info("Starting a simple HTTP server for exposing internal metrics..");
+//            httpServer = new HTTPServer(config.getMetricsPort());
+//            log.info("{}metrics server{} listens on port {}", AnsiColors.ye(), AnsiColors.xx(), config.getMetricsPort());
+//            // todo: create also the service and for openshift also expose the service (?)
+//        } catch (IOException e) {
+//            log.error("Can't start metrics server because of: {} ", e.getMessage());
+//            e.printStackTrace();
+//        }
+//        if (config.isMetricsJvm()) {
+//            DefaultExports.initialize();
+//        }
+//        final Optional<HTTPServer> maybeServer = Optional.of(httpServer);
+//        return CompletableFuture.supplyAsync(() -> maybeServer);
+//    }
 
     private static CompletableFuture<Void> runForNamespace(boolean isOpenShift, String namespace, long reconInterval, int delay) {
+        log.error("cajk9");
         List<ClassLoader> classLoadersList = new LinkedList<>();
         classLoadersList.add(ClasspathHelper.contextClassLoader());
         classLoadersList.add(ClasspathHelper.staticClassLoader());
 
         final List<Class<? extends AbstractOperator>> operatorClasses = new ArrayList<>();
         try {
+            operatorClasses.add((Class<? extends AbstractOperator>) Class.forName("io.radanalytics.operator.cluster.SparkClusterOperator"));
+            log.error("cajk9.3");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            log.error("cajk9.4");
+        }
+        try {
+            log.error("cajk9.5");
             Reflections reflections = new Reflections(new ConfigurationBuilder()
                     .setScanners(new TypeAnnotationsScanner(), new SubTypesScanner(false), new ResourcesScanner())
                     .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0]))));
+            log.error("cajk9.8");
             operatorClasses.addAll(reflections.getSubTypesOf(AbstractOperator.class));
+            log.error("cajk9.9");
         } catch (ReflectionsException re) {
             log.debug(re.getMessage());
+            log.error("cajk10");
             // np, swallow
         }
 
+        log.error("cajk11");
+
         List<Future> futures = new ArrayList<>();
         final int operatorNumber = operatorClasses.size();
+        log.error("cajk12 operatorNumber: " + operatorNumber + "  classes: " + operatorClasses);
         IntStream.range(0, operatorNumber).forEach(operatorIndex -> {
+            log.error("cajk13");
             final Class<? extends AbstractOperator> operatorClass = operatorClasses.get(operatorIndex);
             try {
                 if (!AbstractOperator.class.isAssignableFrom(operatorClass)) {
@@ -146,6 +179,8 @@ public class Entrypoint {
                     log.info("Skipping initialization of {} operator", operatorClass);
                     return;
                 }
+
+                log.error("cajk14");
                 operator.setClient(client);
                 operator.setNamespace(namespace);
                 operator.setOpenshift(isOpenShift);
@@ -177,46 +212,49 @@ public class Entrypoint {
                 futures.add(future);
 //                futures.add(scheduledFuture);
             } catch (InstantiationException | IllegalAccessException e) {
+                log.error("cajk16: " + e);
                 e.printStackTrace();
             }
         });
+        log.error("cajk15");
         return CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{}));
     }
 
     private static boolean isOnOpenShift() {
-        URL kubernetesApi = client.getMasterUrl();
-
-        HttpUrl.Builder urlBuilder = new HttpUrl.Builder();
-        urlBuilder.host(kubernetesApi.getHost());
-
-        if (kubernetesApi.getPort() == -1) {
-            urlBuilder.port(kubernetesApi.getDefaultPort());
-        } else {
-            urlBuilder.port(kubernetesApi.getPort());
-        }
-        if (kubernetesApi.getProtocol().equals("https")) {
-            urlBuilder.scheme("https");
-        }
-        urlBuilder.addPathSegment("oapi");
-
-        OkHttpClient httpClient = HttpClientUtils.createHttpClient(new ConfigBuilder().build());
-        HttpUrl url = urlBuilder.build();
-        Response response;
-        try {
-            response = httpClient.newCall(new Request.Builder().url(url).build()).execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.error("Failed to distinguish between Kubernetes and OpenShift");
-            log.warn("Let's assume we are on K8s");
-            return false;
-        }
-        boolean success = response.isSuccessful();
-        if (success) {
-            log.info("{} returned {}. We are on OpenShift.", url, response.code());
-        } else {
-            log.info("{} returned {}. We are not on OpenShift. Assuming, we are on Kubernetes.", url, response.code());
-        }
-        return success;
+        return true;
+//        URL kubernetesApi = client.getMasterUrl();
+//
+//        HttpUrl.Builder urlBuilder = new HttpUrl.Builder();
+//        urlBuilder.host(kubernetesApi.getHost());
+//
+//        if (kubernetesApi.getPort() == -1) {
+//            urlBuilder.port(kubernetesApi.getDefaultPort());
+//        } else {
+//            urlBuilder.port(kubernetesApi.getPort());
+//        }
+//        if (kubernetesApi.getProtocol().equals("https")) {
+//            urlBuilder.scheme("https");
+//        }
+//        urlBuilder.addPathSegment("oapi");
+//
+//        OkHttpClient httpClient = HttpClientUtils.createHttpClient(new ConfigBuilder().build());
+//        HttpUrl url = urlBuilder.build();
+//        Response response;
+//        try {
+//            response = httpClient.newCall(new Request.Builder().url(url).build()).execute();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            log.error("Failed to distinguish between Kubernetes and OpenShift");
+//            log.warn("Let's assume we are on K8s");
+//            return false;
+//        }
+//        boolean success = response.isSuccessful();
+//        if (success) {
+//            log.info("{} returned {}. We are on OpenShift.", url, response.code());
+//        } else {
+//            log.info("{} returned {}. We are not on OpenShift. Assuming, we are on Kubernetes.", url, response.code());
+//        }
+//        return success;
     }
 
     private static void printInfo() {
@@ -279,5 +317,35 @@ public class Entrypoint {
         InstrumentedAppender metricsLogAppender = new InstrumentedAppender();
         metricsLogAppender.setName("metrics");
         rootLogger.addAppender(metricsLogAppender);
+    }
+
+    public static ExecutorService getExecutors() {
+        if (null == executors) {
+            executors = Executors.newFixedThreadPool(10);
+        }
+        return executors;
+    }
+
+    private static OkHttpClient getOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final X509TrustManager trustAllCerts = new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[]{};
+                }
+            };
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, new X509TrustManager[]{trustAllCerts}, new SecureRandom());
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, trustAllCerts);
+            builder.hostnameVerifier((hostname, session) -> true);
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
